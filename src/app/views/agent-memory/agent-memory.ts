88d3211acca6cac5125fec3541401715fd38ac2f
ikcDevelopment <estuardo.wyss@yahoo.com>
2026-03-01 16:28:23 -0600

    db for llm and memory

--- src/app/views/agent-memory/agent-memory.ts
+++ src/app/views/agent-memory/agent-memory.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input, SimpleChanges} from '@angular/core';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AgentProjectService} from '../../services/memory/agent-project/agent-project-service';
+import {MemoryModel} from '../../services/memory/agent-project/memory-model';
 
 @Component({
   selector: 'app-agent-memory',
@@ -12,7 +13,9 @@ import {AgentProjectService} from '../../services/memory/agent-project/agent-pro
   styleUrl: './agent-memory.css',
 })
 export class AgentMemory {
+    @Input() chatId:string='';
     componentTitle: string = 'Agent Memory';
+    msg:string = '';
 
     form = new FormGroup({
         'chatId': new FormControl('', [Validators.required, Validators.minLength(5)]),
@@ -24,12 +27,38 @@ export class AgentMemory {
     ) {
     }
 
-    submit(){
+    /**
+     * These properties are sent from any component that needs setting users
+     *
+     * first gets into ngOnChanges and then on ngOnInit
+     * @param changes
+     */
+    ngOnChanges(changes: SimpleChanges): void {
+        let propsRead: number = 0;
 
+        for (const propName in changes) {
+            this.form.controls.chatId.setValue(this.chatId);
+        }
     }
 
-    uploadMemory(){}
+    submit(){
+        if(this.form.dirty){
+            if (this.form.status == 'VALID') {
+                this.projectInMemorDb.addUpdateMemory(this.chatId, this.createMemoryModel());
+                this.msg = this.projectInMemorDb.msg;
+            }
+        }
+    }
 
-    private createMemoryModel(){}
+    private uploadMemory(){}
+
+    private createMemoryModel():MemoryModel{
+        const body:MemoryModel={
+            chatId: this.form.controls.chatId.value!,
+            memoryType: this.form.controls.memoryType.value!
+        };
+
+        return body;
+    }
 
 }
--- src/app/views/agent-model/agent-model.ts
+++ src/app/views/agent-model/agent-model.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input, SimpleChanges} from '@angular/core';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AgentProjectService} from '../../services/memory/agent-project/agent-project-service';
+import {LlmModel} from '../../services/memory/agent-project/llm-model';
 
 @Component({
   selector: 'app-agent-model',
@@ -12,7 +13,9 @@ import {AgentProjectService} from '../../services/memory/agent-project/agent-pro
   styleUrl: './agent-model.css',
 })
 export class AgentModel {
+    @Input() chatId:string='';
     componentTitle: string = 'Agent Model';
+    msg:string = '';
 
     form = new FormGroup({
         'chatId': new FormControl('', [Validators.required, Validators.minLength(5)]),
@@ -23,11 +26,38 @@ export class AgentModel {
     constructor( private projectInMemorDb:AgentProjectService) {
     }
 
-    submit(){
+    /**
+     * These properties are sent from any component that needs setting users
+     *
+     * first gets into ngOnChanges and then on ngOnInit
+     * @param changes
+     */
+    ngOnChanges(changes: SimpleChanges): void {
+        let propsRead: number = 0;
 
+        for (const propName in changes) {
+            this.form.controls.chatId.setValue(this.chatId);
+        }
+    }
+
+    submit(){
+        if(this.form.dirty){
+            if (this.form.status == 'VALID') {
+                this.projectInMemorDb.addUpdateModel(this.chatId, this.createLlmModel());
+                this.msg = this.projectInMemorDb.msg;
+            }
+        }
     }
 
     uploadLlmModel(){}
 
-    private createLlmModel(){}
+    private createLlmModel():LlmModel{
+        const body: LlmModel={
+            chatId: this.form.controls.chatId.value!,
+            modelo: this.form.controls.modelo.value!,
+            temperature: this.form.controls.temperature.value!
+        }
+
+        return body;
+    }
 }
